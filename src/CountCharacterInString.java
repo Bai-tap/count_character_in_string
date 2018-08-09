aa8142ffc25d1eabfda218a95b42ad6d8808aee7
longsutls <longsutls@gmail.com>
2018-08-09 19:53:20 +0700

    duyệt ký tự trong chuỗi

--- src/CountCharacterInString.java
+++ src/CountCharacterInString.java
@@ -4,8 +4,15 @@ public class CountCharacterInString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập vào 1 chuỗi ký tự để đếm số lần xuất hiện từng ký tự: ");
-        String characterString = sc.next();
+        String charString = sc.next();
+        System.out.println("Nhập vào ký tự muốn đếm: ");
+        String charCount = sc.next();
 
-
+        int count = 0;
+        for (int i = 0; i < charString.length(); i++) {
+            if (charCount.equals(charString.charAt(i)))
+                count++;
+        }
+        System.out.println(count);
     }
 }
