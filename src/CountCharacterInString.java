79994755de155273a35e8e5f09d362c5767f63d7
longsutls <longsutls@gmail.com>
2018-08-09 11:17:47 +0700

    Nhập chuỗi ký tự từ bàn phím

---
+++ src/CountCharacterInString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CountCharacterInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào 1 chuỗi ký tự để đếm số lần xuất hiện từng ký tự: ");
+        String characterString = sc.next();
+
+
+    }
+}
