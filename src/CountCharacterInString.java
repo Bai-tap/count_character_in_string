a0046ab401e9e1fb3a5768595bd914a2f2342426
longsutls <longsutls@gmail.com>
2018-08-09 20:55:53 +0700

    in ra kết quả. Done

--- src/CountCharacterInString.java
+++ src/CountCharacterInString.java
@@ -10,9 +10,9 @@ public class CountCharacterInString {
 
         int count = 0;
         for (int i = 0; i < charString.length(); i++) {
-            if (charCount.equals(charString.charAt(i)))
+            if (charCount.equals(String.valueOf(charString.charAt(i))))
                 count++;
         }
-        System.out.println("Số lần xuất hiện của ký tự: " + charCount + " là: " + count + " lần.");
+        System.out.println("Số lần xuất hiện của ký tự '" + charCount + "' là: " + count + " lần.");
     }
 }
